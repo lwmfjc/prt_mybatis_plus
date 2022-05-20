2ec1e901aed79681ba9f2a2cbb177f1f6a766e3e
lwmfjc <lwmfjc@gmail.com>
2022-05-20 17:27:34 +0800

    mybatis-plus 练习

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,25 @@
+import com.baomidou.mybatisplus.samples.quickstart.Application;
+import com.baomidou.mybatisplus.samples.quickstart.entity.User;
+import com.baomidou.mybatisplus.samples.quickstart.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest(classes = {Application.class})
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assertions.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+}
