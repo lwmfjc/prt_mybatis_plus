03c786832cb56fc541e8202eeb59513983b0184a
lwmfjc <lwmfjc@gmail.com>
2022-05-26 15:43:10 +0800

    mybatis-plus 练习

--- src/main/java/com/baomidou/mybatisplus/samples/quickstart/entity/User.java
+++ src/main/java/com/baomidou/mybatisplus/samples/quickstart/entity/User.java
@@ -1,11 +1,35 @@
 package com.baomidou.mybatisplus.samples.quickstart.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "`user`")
 public class User {
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id", type = IdType.INPUT)
     private Long id;
+
+    /**
+     * 姓名
+     */
+    @TableField(value = "`name`")
     private String name;
+
+    /**
+     * 年龄
+     */
+    @TableField(value = "age")
     private Integer age;
+
+    /**
+     * 邮箱
+     */
+    @TableField(value = "email")
     private String email;
 }
--- src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
+++ src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
@@ -2,7 +2,8 @@ package com.baomidou.mybatisplus.samples.quickstart.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
-
 }
